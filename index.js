aabff9d926eb9d90ccd82eeb41581f70b193d50f
Mitchell Burton <mitch@mitchellburton.ca>
2023-06-14 15:34:01 -0700

    add some controls

--- index.js
+++ index.js
@@ -7,60 +7,78 @@ window.addEventListener("DOMContentLoaded", () => {
         major: ["I", null,  "ii",  null, "iii", "IV", null, "V", null, "vi",  null, "viio"],
         minor: ["i", null, "iio", "III",  null, "iv", null, "v", "VI", null, "VII",   null],
     };
-
     const NOTES = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
-    const ROOTED = true;
-    const CHORD_COUNT = 4;
 
-    const scaleKeys = Object.keys(SCALES);
-    const scaleKey = scaleKeys[Math.floor(Math.random() * scaleKeys.length)];
-    const scale = SCALES[scaleKey];
-    const numerals = NUMERALS[scaleKey];
-    const keyI = Math.floor(Math.random() * NOTES.length);
-    const root = NOTES[keyI];
+    const rootedCheckbox = document.querySelector("#rooted");
+    const chordCountInput = document.querySelector("#chord-count");
+    const incDimCheckbox = document.querySelector("#inc-dim");
+    const body = document.querySelector("#output");
 
-    const prog = [];
-    const progChords = [];
-    let chordCount = CHORD_COUNT;
+    const generateChords = () => {
+        const rooted = rootedCheckbox.checked;
+        const incDim = incDimCheckbox.checked;
 
-    if (ROOTED) {
-        prog.push(numerals[0]);
-        progChords.push(root + scale[0]);
-        chordCount--;
-    }
+        const scaleKeys = Object.keys(SCALES);
+        const scaleKey = scaleKeys[Math.floor(Math.random() * scaleKeys.length)];
+        const scale = SCALES[scaleKey];
+        const numerals = NUMERALS[scaleKey];
+        const keyI = Math.floor(Math.random() * NOTES.length);
+        const root = NOTES[keyI];
 
-    while (chordCount) {
-        const next = Math.floor(Math.random() * NOTES.length);
-        const nextNote = (next + keyI) % NOTES.length;
+        const prog = [];
+        const progChords = [];
+        let chordCountdown = parseInt(chordCountInput.value);
 
-        if (scale[next] !== null) {
+        if (rooted) {
+            prog.push(numerals[0]);
+            progChords.push(root + scale[0]);
+            chordCountdown--;
+        }
+
+        while (chordCountdown) {
+            const next = Math.floor(Math.random() * NOTES.length);
+            const nextNote = (next + keyI) % NOTES.length;
+
+            if (scale[next] === null) {
+                continue;
+            }
+
+            if (!incDim && scale[next] === "dim") {
+                continue;
+            }
+
             prog.push(numerals[next]);
             progChords.push(NOTES[nextNote] + scale[next]);
-            chordCount--;
+            chordCountdown--;
         }
-    }
 
-    const leadP = document.createElement("p");
-    leadP.innerText = "You should write a song using this fucking chord progression.";
-    const scaleP = document.createElement("p");
-    scaleP.innerText = `${root} ${scaleKey}`;
+        Array.from(body.childNodes).forEach(c => c.remove());
 
-    const progDiv = document.createElement("div");
-    prog.forEach((p) => {
-        const progP = document.createElement("p");
-        progP.innerText = p;
-        progDiv.append(progP);
-    });
-    progDiv.classList.add("flex");
+        const leadP = document.createElement("p");
+        leadP.innerText = "You should write a song using this fucking chord progression:";
+        const scaleP = document.createElement("p");
+        scaleP.innerText = `${root} ${scaleKey}`;
 
-    const progChordsDiv = document.createElement("div");
-    progChords.forEach((progChord) => {
-        const progChordP = document.createElement("p");
-        progChordP.innerText = progChord;
-        progChordsDiv.append(progChordP);
-    });
-    progChordsDiv.classList.add("flex");
+        const progDiv = document.createElement("div");
+        prog.forEach((p) => {
+            const progP = document.createElement("p");
+            progP.innerText = p;
+            progDiv.append(progP);
+        });
+        progDiv.classList.add("flex");
 
-    const body = document.querySelector("body");
-    body.append(leadP, scaleP, progDiv, progChordsDiv);
+        const progChordsDiv = document.createElement("div");
+        progChords.forEach((progChord) => {
+            const progChordP = document.createElement("p");
+            progChordP.innerText = progChord;
+            progChordsDiv.append(progChordP);
+        });
+        progChordsDiv.classList.add("flex");
+
+        body.append(leadP, scaleP, progDiv, progChordsDiv);
+    };
+
+    document.querySelector("#regen").addEventListener("click", () => generateChords());
+
+    generateChords(rootedCheckbox.value === "on", parseInt(chordCountInput.value));
 });
